0c36a5e8f03d1f9a04f3dae32c73f42660f27b98
Tomasz Jankowski <tomasz.jankowski.mail@gmail.com>
2019-05-01 23:14:26 +0200

    Add support for generating symbolic and numeric equations

--- eqtex.py
+++ eqtex.py
@@ -34,26 +34,35 @@ class _NodeVisitor(ast.NodeVisitor):
             raise RuntimeError(f'{name}() not found!')
 
     def process_Name(self, val):
-        return val.id
+        return val.id, self.tokens.get(val.id,val.id)
 
     def process_Num(self, val):
-        return str(val.n)
+        return str(val.n), str(val.n)
 
     def process_numpy_invert(self, args):
-        return '{' + args[0].id + '}^{-1}'
+        sym = args[0].id
+        val = self.tokens.get(sym, sym)
+
+        return '{' + sym + '}^{-1}', \
+               '{' + val + '}^{-1}'
+
 
     def process_numpy_array(self, args):
-        rows = []
+        sym_rows = []
+        val_rows = []
 
         if isinstance(args[0].elts[0], ast.List):
             for row in args[0].elts:
-                vals = [self.process(val) for val in row.elts]
-                rows.append('&'.join(vals))
+                syms, vals = zip(*[self.process(val) for val in row.elts])
+                sym_rows.append('&'.join(syms))
+                val_rows.append('&'.join(vals))
         else:
-            vals = [self.process(val) for val in args[0].elts]
-            rows.append('&'.join(vals))
+            syms, vals = zip(*[self.process(val) for val in args[0].elts])
+            sym_rows.append('&'.join(syms))
+            val_rows.append('&'.join(vals))
 
-        return r'\begin{bmatrix}' + r'\\'.join(rows) + r'\end{bmatrix}'
+        return r'\begin{bmatrix}' + r'\\'.join(sym_rows) + r'\end{bmatrix}', \
+               r'\begin{bmatrix}' + r'\\'.join(val_rows) + r'\end{bmatrix}'
 
     def process_Call(self, val):
         return self.process(val.args, func_suffix=f'numpy_{val.func.id}', ignore_missing=False)
@@ -63,25 +72,35 @@ class _NodeVisitor(ast.NodeVisitor):
         r = self.process(val.right)
 
         if isinstance(val.left, ast.BinOp):
+            l_sym, l_val = l
             if (self.get_precedense(val.left.op) < self.get_precedense(val.op)) and not isinstance(val.op, ast.Div):
-                l = r'\left(' + l + r'\right)'
+                l = (r'\left(' + l_sym + r'\right)', r'\left(' + l_val + r'\right)')
         if isinstance(val.right, ast.BinOp):
+            r_sym, r_val = r
             if (self.get_precedense(val.right.op) < self.get_precedense(val.op)) and not isinstance(val.op, ast.Div):
-                r = r'\left(' + r + r'\right)'
-
+                r = (r'\left(' + r_sym + r'\right)', r'\left(' + r_val + r'\right)')
         return self.process(val.op, l, r)
 
     def process_Pass(self, _):
         pass
 
     def process_Mult(self, _, l, r):
-        return l + r' \cdot ' + r
+        l_sym, l_val = l
+        r_sym, r_val = r
+        return l_sym + r' \cdot ' + r_sym,\
+               l_val + r' \cdot ' + r_val
 
     def process_Sub(self, _, l, r):
-        return l + r' - ' + r
+        l_sym, l_val = l
+        r_sym, r_val = r
+        return l_sym + r' - ' + r_sym,\
+               l_val + r' - ' + r_val
 
     def process_Div(self, _, l, r):
-        return r'\frac{' + l + r'}{' + r + r'}'
+        l_sym, l_val = l
+        r_sym, r_val = r
+        return r'\frac{' + l_sym + r'}{' + r_sym + r'}', \
+               r'\frac{' + l_val + r'}{' + r_val + r'}'
 
     def process_Assign(self, stmt):
         if len(stmt.targets) > 2:
@@ -91,9 +110,9 @@ class _NodeVisitor(ast.NodeVisitor):
 
         for target, val in zip(stmt.targets, vals):
             name = target.id
-            val = self.process(val)
+            sym, val = self.process(val)
             self.tokens[name] = val
-            return f'{name}={val}'
+            return f'{name}={sym}', f'{name}={val}'
 
     def visit_FunctionDef(self, node):
         if len(node.decorator_list) == 0:
@@ -103,8 +122,12 @@ class _NodeVisitor(ast.NodeVisitor):
         doc.packages.append(Package('amsmath'))
 
         for stmt in node.body:
+            sym, val = self.process(stmt)
             doc.append(NoEscape(r'\begin{equation}'))
-            doc.append(NoEscape(self.process(stmt)))
+            doc.append(NoEscape(sym))
+            doc.append(NoEscape(r'\end{equation}'))
+            doc.append(NoEscape(r'\begin{equation}'))
+            doc.append(NoEscape(val))
             doc.append(NoEscape(r'\end{equation}'))
 
         doc.generate_pdf('numpy_ex', clean_tex=False)
