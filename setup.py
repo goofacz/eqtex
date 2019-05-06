3e9c8a583c5042442f5e4ad773afe88f9f56c095
Tomasz Jankowski <tomasz.jankowski.mail@gmail.com>
2019-05-06 18:17:33 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,35 @@
+# This file is part of EqTex.
+#
+# Copyright 2019 Tomasz Jankowski
+#
+# EqTex is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# EqTex is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser Public License for more details.
+#
+# You should have received a copy of the GNU Lesser Public License
+# along with EqTex. If not, see <http://www.gnu.org/licenses/>.
+
+from setuptools import setup
+
+setup(
+    name='eqtex',
+    version='0.1',
+    description='EqTex is a Python module for transforming while functions with math equations to LaTeX',
+    url='https://github.com/goofacz/eqtex',
+    author='Tomasz Jankowski',
+    author_email='tomasz.jankowski.mail@gmail.com',
+    keywords='python scientific latex numpy',
+    python_requires='>=3.0',
+    extras_require={
+        'test': ['numpy'],
+    },
+
+    # TODO
+    # - classifiers
+)
